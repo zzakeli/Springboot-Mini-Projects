5f13fc31af4d7bd4b1f1d1707778d111ffdae137
Jzy <jestalycastillo15@gmail.com>
2025-06-24 20:47:01 +0800

    Add: Auth System

---
+++ Authentication-System/auth-system/src/main/java/com/authsystem/auth_system/AuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.authsystem.auth_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSystemApplication.class, args);
+	}
+
+}
---
+++ Authentication-System/auth-system/src/test/java/com/authsystem/auth_system/AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authsystem.auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
